d2a978f4148b7e385b97f10e23295d575b6e5b55
TheAnswer07 <moisedia7@gmail.com>
2022-09-21 14:29:07 -0700

    Files added and initial tests for the application done

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-for (let i = 0; i < 10; i++) {
-    console.log("Hello OOP + Node JS World");
-}
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/Renderer");
+
+
+let employeeArray = [];
+
+inquirer.prompt([
+    {
+        message: "What is the name of the Manager of this team?",
+        type: "input",
+        name: "name"
+    },
+    {
+        message: "What is the id of the Manager of this team?",
+        type: "input",
+        name: "id"
+    },
+    {
+        message: "What is the email of the Manager of this team?",
+        type: "input",
+        name: "email"
+    },
+    {
+        message: "What is the office number of the Manager of this team?",
+        type: "input",
+        name: "officeNumber"
+    }
+])
+.then(manager => {
+    console.log(manager);
+    let newManager = new Manager(manager.name, manager.id, manager.email, manager.officeNumber);
+    employeeArray.push(newManager);
+    console.log(employeeArray);
+});
