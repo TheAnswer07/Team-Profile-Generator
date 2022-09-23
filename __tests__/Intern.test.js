978ce5e1dd2eb466eed651a97ebc49b299af1f35
TheAnswer07 <moisedia7@gmail.com>
2022-09-22 19:15:22 -0700

    Intern test completed

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern");
+
+test("Can set School account via constructor", () => {
+    const testValue = "UCB";
+    const e = new Intern("Hey", 7, "md@gmail.com", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Hey", 7, "md@gmail.com", "UCB");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get School via getSchool()", () => {
+    const testValue = "UCB";
+    const e = new Intern("Hey", 7, "md@gmail.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
