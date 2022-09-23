45a09a90e843f915b71e7953eb3998c6f2ab9949
TheAnswer07 <moisedia7@gmail.com>
2022-09-22 22:19:31 -0700

    Test running perfectly and application showing proper interface with inputted data

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,17 +3,17 @@ const Engineer = require("../lib/Engineer");
 test("Can set GitHub account via constructor", () => {
     const testValue = "GitHubUser";
     const e = new Engineer("Hey", 7, "md@gmail.com", testValue);
-    expect(e.github).toBe(testValue);
+    expect(e.getGithub()).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
-    const testValue = "Engineer";
-    const e = new Engineer("Hey", 7, "md@gmail.com", "GitHubUser");
-    expect(e.getRole()).toBe(testValue);
-});
-
-test("Can get GitHub username via getGithub()", () => {
     const testValue = "GitHubUser";
     const e = new Engineer("Hey", 7, "md@gmail.com", testValue);
-    expect(e.getRole()).toBe(testValue);
+    expect(e.getRole()).toBe("Engineer");
+});
+
+test("Can get Email via getEmail", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Hey", 7, "md@gmail.com", testValue);
+    expect(e.getEmail()).toBe("md@gmail.com");
 });
